dc83bfb8806ff896e6b2884c8c9eec4aa5e5f39c
coolbreeze822 <lroscoez82289@gmail.com>
2019-02-28 16:21:13 -0600

    ActiveDirectory_DisabledReport_[OC]
    ## Paths to files, Organizational Unit(to the disabled computers container within your OU), email address/smpt information will need to be updated by you before running##
    
    Goal behind this was to easily pull a list of all the computers(for this purpose) from the disabled OU along with when they were moved to the disabled OU.  Luckily for me, the date they were moved, along with the OU they originated from is all stored in the description. It took a couple hours of googling to find a script(Save-CSVasEXCEL is not mine, thank you to the original author, gangstanthony), and the rest was kinda easy to throw together.
    
    As far as where I would like to go with this from here, I think it would be cool to add some excel formatting(main reason for needing to save as .xlsx)  so that I can highlight ones that meet certain criteria...etc

---
+++ DisabledReport.ps1
@@ -0,0 +1,63 @@
+﻿function Save-CSVasExcel {
+    param (
+        [string]$CSVFile = $(Throw 'No file provided.')
+    )
+    
+    BEGIN {
+        function Resolve-FullPath ([string]$Path) {    
+            if ( -not ([System.IO.Path]::IsPathRooted($Path)) ) {
+                # $Path = Join-Path (Get-Location) $Path
+                $Path = "$PWD\$Path"
+            }
+            [IO.Path]::GetFullPath($Path)
+        }
+
+        function Release-Ref ($ref) {
+            ([System.Runtime.InteropServices.Marshal]::ReleaseComObject([System.__ComObject]$ref) -gt 0)
+            [System.GC]::Collect()
+            [System.GC]::WaitForPendingFinalizers()
+        }
+        
+        $CSVFile = Resolve-FullPath $CSVFile
+        $xl = New-Object -ComObject Excel.Application
+    }
+
+    PROCESS {
+        $wb = $xl.workbooks.open($CSVFile)
+        $xlOut = $CSVFile -replace '\.csv$', '.xlsx'
+        
+        # can comment out this part if you don't care to have the columns autosized
+        $ws = $wb.Worksheets.Item(1)
+        $range = $ws.UsedRange 
+        [void]$range.EntireColumn.Autofit()
+
+        $num = 1
+        $dir = Split-Path $xlOut
+        $base = $(Split-Path $xlOut -Leaf) -replace '\.xlsx$'
+        $nextname = $xlOut
+        while (Test-Path $nextname) {
+            $nextname = Join-Path $dir $($base + "-$num" + '.xlsx')
+            $num++
+        }
+
+        $wb.SaveAs($nextname, 51)
+    }
+
+    END {
+        $xl.Quit()
+    
+        $null = $ws, $wb, $xl | % {Release-Ref $_}
+
+        # del $CSVFile
+    }
+}
+$currentDate = Get-Date
+$currentDate = $currentDate.ToString('MM-dd-yyyy_hh-mm-ss')
+$file = "C:\scriptReports\DisabledPCs_$currentDate" + ".csv"
+Get-ADComputer -Filter * -SearchBase "OU=Disabled_Computers,OU=Computers,OU=,DC=something,DC=dot,DC=com" -Properties Description | select Name,Description |Export-CSV -Path $file -NoTypeInformation
+Save-CSVasExcel $file
+$newfile = $file -replace '\.csv$', '.xlsx'
+Remove-Item $file
+Send-MailMessage -To “coolbreeze822@email.com" -From “NO-REPLY@email.com" -SMTPServer mail.email.com -Subject “Disabled Computers” -Body “Please review this weeks report of Computers in the Disabled_Computer OU and do what you can to help clean it out. `n `n Please Do Not Respond To This Email `n `n Thank you ” -Attachments $newfile -Priority High
+
+
